b6374637ebca92fd8c4d3ba28a19de648acd8f1d
PrestonChen <tianpei.chen@mail.mcgill.ca>
2014-10-27 19:17:25 -0400

    add a folder SIMULATION

--- FCSD_BER_CPU/Debug/src/subdir.mk
+++ FCSD_BER_CPU/Debug/src/subdir.mk
@@ -3,6 +3,10 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../src/FCSD_detection_CPU.o \
+../src/fullfactorial.o 
+
 CPP_SRCS += \
 ../src/FCSD_detection_CPU.cpp \
 ../src/communication_line.cpp \

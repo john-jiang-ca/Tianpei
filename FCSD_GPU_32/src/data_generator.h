ff140761b16098c21b9eb8b4ffd159d36c82530d
PrestonChen <tianpei.chen@mail.mcgill.ca>
2014-11-11 15:18:01 -0500

    revise the speedup table

---
+++ FCSD_GPU_32/src/data_generator.h
@@ -0,0 +1,13 @@
+
+void data_generator(gsl_vector_ulong *pdata, gsl_rng *pr, unsigned long Q)
+{
+	int Nt;
+	Nt = pdata -> size;	
+	int count = 0;
+	for (count=0; count<Nt ; count++)
+	  {
+		gsl_vector_ulong_set(pdata, count, gsl_rng_uniform_int (pr,Q));	
+		/*printf ("\n data(%d)= %d \n", count, gsl_vector_ulong_get (pdata, count));*/	
+	  }
+	return;
+}

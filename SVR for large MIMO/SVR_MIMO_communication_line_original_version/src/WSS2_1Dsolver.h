fc76f52009731e737548b9e0c83110def933f44f
PrestonChen <tianpei.chen@mail.mcgill.ca>
2015-08-11 01:02:45 -0400

    code backup

---
+++ SVR for large MIMO/SVR_MIMO_communication_line_original_version/src/WSS2_1Dsolver.h
@@ -0,0 +1,74 @@
+/*
+ * WSS2_1Dsolver.h
+ *
+ *  Created on: Aug 4, 2015
+ *      Author: tianpei.chen@mail.mcgill.ca
+ *      This is 2D selection subroutine based on maximum 2 1D direction
+ *      subroutine, choose the two Lagrange multipliers with maximum 1D gain
+ *      to perform 2D solver.
+ *      INPUT: alpha(beta), alpha_hat(beta_hat), label1, label2, phi, R_Kernel
+ *      OUTPUT: First, Second
+ */
+#include "common.h"
+#ifndef WSS2_1DSOLVER_H_
+#define WSS2_1DSOLVER_H_
+void 	WSS2_1Dsolver(gsl_vector *l_m,  //Lagrange Multiplier vector
+        gsl_vector_complex *phi,  //update parameter
+        gsl_matrix *R_Kernel,    //real kernel matrix
+        int F_i,  //index of first maximum Lagrange multiplier
+        int S_i, //index of second maximum Lagrange multiplier
+		int model //determine this routine is for real part (0) or imaginary part (1)
+){
+double best_g, g;
+double sigma, sigma_hat;
+double temp_l_m, temp_l_m_hat;
+int count;
+//double phi_p[2*Nr];
+//for(count=0;count<2*Nr;count++){  //take real and imaginary part of phi
+//	if(count<Nr){
+//  phi_p[count]=gsl_vector_complex_get(phi,count).dat[0];
+//	}else{
+//		phi_p[count]=gsl_vector_complex_get(phi,count-Nr).dat[1];
+//	}
+//}
+best_g=-1;
+F_i=S_i=-1;
+sigma=0;
+sigma_hat=0;
+for(count=0;count<2*Nr;count++){
+	if(count<Nr){
+		temp_l_m=gsl_vector_get(l_m, count)+(gsl_vector_complex_get(phi,count).dat[model]-epsilon)/(gsl_matrix_get(R_Kernel,count,count));
+		if(temp_l_m<0){
+			temp_l_m=0;
+		}else if (temp_l_m>C){
+			temp_l_m=C;
+		}
+
+		sigma=temp_l_m-gsl_vector_get(l_m,count);
+		sigma_hat=0;
+	}else{
+		temp_l_m_hat=gsl_vector_get(l_m, count)-double((gsl_vector_complex_get(phi,count).dat[model]+epsilon))/double((gsl_matrix_get(R_Kernel, count,count)));
+		if(temp_l_m_hat<0){
+			temp_l_m_hat=0;
+		}else if (temp_l_m_hat>C){
+			temp_l_m_hat=C;
+		}
+		sigma=0;
+		sigma_hat=temp_l_m_hat-gsl_vector_get(l_m,count);
+	}
+
+	g=abs((sigma-sigma_hat)*(-0.5*(sigma-sigma_hat)*gsl_matrix_get(R_Kernel(count,count))+gsl_vector_complex_get(phi, count).dat[model]-epsilon*(sigma+sigma_hat)/(sigma-sigma_hat)));
+	if(g>best_g){
+		best_g=g;
+		S_i=F_i;
+		F_i=count;
+	}
+}
+
+
+}
+
+
+
+
+#endif /* WSS2_1DSOLVER_H_ */
---
+++ SVR for large MIMO/SVR_MIMO_communication_line_original_version/src/grayencoder.h
@@ -0,0 +1,67 @@
+void grayencoder (gsl_vector_ulong *pgraydata, gsl_vector_ulong *pgrayindexes, unsigned long Q)
+{
+	unsigned long count=0UL, value=0UL, count2=0UL, temp=0UL;
+
+	int M=0;
+	M = (int) ceil(sqrt(Q));
+
+	gsl_vector_ulong *ppamdata, *pQAMdata, *peqvdata;
+	ppamdata = gsl_vector_ulong_calloc (M);
+	pQAMdata = gsl_vector_ulong_calloc (Q);
+	peqvdata = gsl_vector_ulong_calloc (Q);
+if(Q==4)
+{
+ for (count=0;count<Q;count++)
+ {
+	 switch (count) {
+	 case 0:
+	 gsl_vector_ulong_set(pQAMdata,count,0); break;
+	 gsl_vector_ulong_set(peqvdata,0,count); break;
+	 case 1:
+		 gsl_vector_ulong_set(pQAMdata,count,1); break;
+		 gsl_vector_ulong_set(peqvdata,1,count); break;
+	 case 2:
+		 gsl_vector_ulong_set(pQAMdata,count,3); break;
+		 gsl_vector_ulong_set(peqvdata,3,count); break;
+	 case 3:
+		 gsl_vector_ulong_set(pQAMdata,count,2); break;
+		 gsl_vector_ulong_set(peqvdata,2,count); break;
+	 }
+ }
+}
+else
+{
+	for (count=0; count<M; count++)
+	  {
+		value=count;
+		count2 = count;
+		count2 = count2 >> 1;
+		value = value^count2;
+		
+		gsl_vector_ulong_set (ppamdata, count, value);
+
+	  }
+
+	//vectorviewerint("ppamdata", ppamdata);
+	
+	for (count=0; count<Q; count++)
+	  {
+		value = M*gsl_vector_ulong_get(ppamdata, count/M) + gsl_vector_ulong_get (ppamdata, count%M);
+		gsl_vector_ulong_set (pQAMdata, count, value);
+		gsl_vector_ulong_set (peqvdata, value, count);   //the pdata is actually the index
+		//according to the gray code we can find its indexs according to the indexes we can find the gray code
+
+	  }
+}
+
+	//vectorviewerint ("pQAMdata", pQAMdata);
+	//vectorviewerint ("peqvdata", peqvdata);
+	gsl_vector_ulong_memcpy (pgraydata, peqvdata);
+	gsl_vector_ulong_memcpy (pgrayindexes, pQAMdata);
+
+	gsl_vector_ulong_free (ppamdata);
+	gsl_vector_ulong_free (pQAMdata);
+	gsl_vector_ulong_free (peqvdata);
+
+	return;
+}

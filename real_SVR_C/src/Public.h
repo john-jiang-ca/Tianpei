83f1d095b8bd31622765f4a814673920743356a7
PrestonChen <tianpei.chen@mail.mcgill.ca>
2015-09-06 22:13:54 -0400

    modified C code and report

--- real_SVR_C/src/Public.h
+++ real_SVR_C/src/Public.h
@@ -19,19 +19,21 @@
 #include<time.h>
 #include<gsl/gsl_linalg.h>
 #define Nr 100
-#define Nt 40
+#define Nt 100
 #define M 4
-#define SNRnum 10
-#define Iteration "iteration.txt"
-#define SER "SER.txt"
-#define BER "BER.txt"
+#define SNRnum 16
+#define Iteration "Iteration100_100_24.txt"
+#define SER "SER100_100_24.txt"
+#define BER "BER100_100_24.txt"
 #define tol 1e-3
 #define epsilon 1e-7
-#define C 1
+#define C 1.0
 #define SIZE 1000000
 #define SVR
+#define begin 12
 //#define MMSE
 //#define DEBUG
+//#define TEST
 
 
 

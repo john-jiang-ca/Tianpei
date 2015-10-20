19a780a5a4f0fe159761b80b5d51ec3eaa736209
PrestonChen <tianpei.chen@mail.mcgill.ca>
2015-10-20 14:05:03 -0400

    matlab code modification

--- SVR for large MIMO/real SVR matlab/CSVR/MIC_ordering_ascend_Recursive.m
+++ SVR for large MIMO/real SVR matlab/CSVR/MIC_ordering_ascend_Recursive.m
@@ -66,7 +66,7 @@ if(stage==maxStage)
     symOut=symOut_current;
     return
 end
-[symOut]=MIC_Recursive(y, H, symOut_current, SNRd, M, pav, stage+1, maxStage, W,G, symConstell);
+[symOut]=MIC_ordering_ascend_Recursive(y, H, symOut_current, SNRd, M, pav, stage+1, maxStage, W,G, symConstell,order);
 end
 
 
@@ -93,62 +93,4 @@ for count1=1:Nt
 end
 end
 
-function [X_hat] = Rectangular_QAM_slicer(X, M, pav)    %slicer
-%need to be modified
-
-% sq10=sqrt(10);
-if(M==2)
-    d=sqrt(pav);
-else
-d=sqrt(3*pav/(2*(M-1)));
-end
-
-N=length(X);
-for i=1:N
-if(M==2)
-    
-    if(real(X(i))<=0)
-        REAL=-d;
-    else
-        REAL=d;
-    end
-    X(i)=complex(REAL,0);
-elseif(M==4)
-        if(real(X(i))<=0)
-        REAL=-d;
-    else
-        REAL=d;
-        end
-        if(imag(X(i))<=0)
-        IMAG=-d;
-    else
-        IMAG=d;
-        end
-        X(i)=complex(REAL, IMAG);
-elseif(M==16)
-    
-        if(real(X(i))<=-2*d)
-        REAL=-3*d;
-        elseif (real(X(i))>-2*d&&real(X(i))<=0)
-        REAL=-d;
-        elseif(real(X(i))>0&&real(X(i))<=2*d)
-            REAL=d;
-        elseif(real(X(i)>2*d))
-            REAL=3*d;
-        end
-        if(imag(X(i))<=-2*d)
-        IMAG=-3*d;
-        elseif (imag(X(i))>-2*d&&imag(X(i))<=0)
-        IMAG=-d;
-        elseif(imag(X(i))>0&&imag(X(i))<=2*d)
-            IMAG=d;
-        elseif(imag(X(i)>2*d))
-            IMAG=3*d;
-        end
-        X(i)=complex(REAL, IMAG);
-end
-
-end
-X_hat=X;
-end
 

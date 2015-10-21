806ce1e9d44169db1fa11823c424696d2f6725ca
PrestonChen <tianpei.chen@mail.mcgill.ca>
2015-10-21 17:55:22 -0400

    Figure of simulation results

--- SVR for large MIMO/real SVR matlab/CSVR/FIGURE/SNR_BER_new.m
+++ SVR for large MIMO/real SVR matlab/CSVR/FIGURE/SNR_BER_new.m
@@ -1,13 +1,52 @@
-SER_MMSE_OSIC_32_4=[0.2972062500, 0.2279687500, 0.1424250000, 0.0705062500, 0.0286875000, 0.0089000000, 0.0020500000, 0.0003006077 ];
-SER_MMSE_OSIC_32_16=[0.7440250000 ,0.7111250000 ,0.6748750000 ,0.6352875000 ,0.5856500000, 0.5062062500, 0.3750312500, 0.2198000000, 0.0967562500, 0.0360125000 ];
-SER_MMSE_OSIC_64_4=[0.3385843750, 0.2814031250 ,0.2082000000, 0.1259750000, 0.0598437500, 0.0233000000, 0.0060375000, 0.0010937500 ];
-SER_MMSE_OSIC_64_16=[0.7628968750, 0.7342406250 ,0.7039031250 ,0.6676812500, 0.6317593750, 0.5807312500 ,0.5181187500 ,0.4029062500 ];
-SER_RSVD_OMIC_32_4=[0.1529875000 ,0.0724187500, 0.0253937500, 0.0111187500, 0.0047312500 ,0.0020437500, 0.0020062500 ,0.0014250000 ];                                                                                                                      
-SER_RSVD_OMIC_32_16=[0.6707875000, 0.6202625000 ,0.5760937500, 0.5362062500, 0.4985500000, 0.4672812500, 0.4484625000, 0.4391375000, 0.4282062500, 0.4167125000 ];
+%% MMSE-OSIC
+SER_MMSE_OSIC_32_4=[0.2972062500, 0.2279687500 ,0.1424250000 ,0.0705062500 ,0.0286875000 ,0.0089000000 ,0.0020500000 ,0.0003006077 ];
+SER_MMSE_OSIC_32_16=[0.7440250000, 0.7111250000, 0.6748750000 ,0.6352875000, 0.5856500000, 0.5062062500 ,0.3750312500,...
+    0.2198000000, 0.0967562500, 0.0360125000];
+SER_MMSE_OSIC_64_4=[0.3385843750, 0.2814031250, 0.2082000000, 0.1259750000, 0.0598437500, 0.0233000000, 0.0060375000, 0.0010937500  ];
+SER_MMSE_OSIC_64_16=[0.7628968750, 0.7342406250, 0.7039031250, 0.6676812500, 0.6317593750, 0.5807312500, 0.5181187500, 0.4029062500,...
+    0.2354906250, 0.1079718750, 0.0338187500, 0.0088281250, 0.0012406250 ];
+%% RSVD-OMIC
+SER_RSVD_OMIC_32_4=[0.1529875000, 0.0724187500, 0.0253937500, 0.0111187500, 0.0047312500 ,0.0020437500, 0.0020062500 ,0.0014250000  ];                                                                                                                      
+SER_RSVD_OMIC_32_16=[0.6707875000, 0.6202625000, 0.5760937500, 0.5362062500, 0.4985500000 ,0.4672812500 ,0.4484625000,...
+    0.4391375000, 0.4282062500 ,0.4167125000  ];
 SER_RSVD_OMIC_64_4=[0.1571250000, 0.0588906250, 0.0090093750, 0.0008875000, 0.0001451598, 0.0000446016 ];
-SER_RSVD_OMIC_64_16=[0.6781468750, 0.6220937500, 0.5678343750, 0.5115250000, 0.4639031250 ,0.4216093750 ,0.3954843750, 0.3695625000 ];
-fig1=figure;
-% Xsize=8.5;Ysize=11;
+SER_RSVD_OMIC_64_16=[0.6781468750 0.6220937500 0.5678343750 0.5115250000 0.4639031250 0.4216093750 0.3954843750 0.3695625000,...
+    0.3585375000, 0.3443187500, 0.3399000000, 0.3332718750, 0.3343687500 ];
+%% MMSE
+SER_MMSE_32_4=[0.3471625000, 0.3011687500, 0.2510812500, 0.2024062500, 0.1565687500, 0.1178125000, 0.0847562500,...
+    0.0618375000, 0.0410187500, 0.0293312500 ,0.0191062500 ];
+SER_MMSE_32_16=[0.7700937500, 0.7424125000, 0.7070312500 ,0.6658000000, 0.6181000000, 0.5622250000,...
+    0.4945250000, 0.4318125000, 0.3537750000 ,0.2838937500, 0.2119875000 ];
+SER_MMSE_64_4=[0.4067812800, 0.3648468780, 0.3187862800, 0.2688406280 ,0.2208128000, 0.1722062800 ,0.1322280000, 0.0987093780,...
+    0.0718893780, 0.0486878000, 0.0341812800 ];
+SER_MMSE_64_16=[0.8025093750, 0.7781718750, 0.7522625000 ,0.7180750000, 0.6829937500, 0.6368843750, 0.5848312500 ,0.5295250000, 0.4625500000,...
+    0.3902406250, 0.3158062500];
+%% RSVD
+SER_RSVD_32_4=[0.2014687500, 0.1390687500 ,0.0961687500, 0.0687187500 ,0.0510500000, 0.0410812500 ,0.0369937500, 0.0336125000,...
+    0.0324375000, 0.0313375000 ];
+SER_RSVD_32_16=[0.6839750000, 0.6338250000, 0.5861437500, 0.5506687500, 0.5166312500, 0.4973375000,...
+    0.4857812500, 0.4803875000, 0.4745750000, 0.4722875000  ];
+SER_RSVD_64_4=[0.2251687500 ,0.1492437500 ,0.0898437500 ,0.0540218750, 0.0334843750 ,0.0232468750 ,0.0180375000 ,0.0154843750 ,0.0141281250 ];
+SER_RSVD_64_16=[0.6988093750 ,0.6368156250, 0.5719250000, 0.5092625000, 0.4619156250, 0.4268875000 ,0.4097125000 ,0.3975875000 ,0.3914562500, 0.3852500000  ];
+%% MMSE-AOMIC
+SER_MMSE_AOMIC_32_4=[0.1993562500, 0.1323125000, 0.0773750000, 0.0477500000, 0.0252062500, 0.0168000000 ,0.0106812500 ,...
+    0.0068750000, 0.0039625000, 0.0034000000  ];
+SER_MMSE_AOMIC_32_16=[0.6985500000, 0.6601375000 ,0.6237375000, 0.5810750000 ,0.5362500000, 0.4748375000,...
+    0.4049687500, 0.3432187500 ,0.2662000000, 0.2043562500, 0.1467312500, 0.1051375000];
+SER_MMSE_AOMIC_64_4=[0.2162437500 ,0.1457968750, 0.0804312500 ,0.0423437500 ,0.0242718750 ,0.0116531250 ,0.0072125000,...
+    0.0054281250, 0.0036656250 ,0.0014718750];
+SER_MMSE_AOMIC_64_16=[0.7130406250, 0.6775250000, 0.6472187500, 0.6113437500, 0.5776875000, 0.5367343750, 0.4889937500,...
+    0.4314718750, 0.3660656250 ,0.2928500000];
+%% MMSE-DOMIC
+SER_MMSE_DOMIC_32_4=[0.1964375000, 0.1202312500, 0.0657812500, 0.0333062500, 0.0162187500 ,0.0084250000, 0.0038812500,...
+    0.0018062500, 0.0011030672, 0.0009252844 ];
+SER_MMSE_DOMIC_32_16=[0.6971125000, 0.6597000000, 0.6186125000 ,0.5712937500, 0.5176750000 ,0.4486437500, 0.3618625000,...
+    0.2878437500, 0.2048812500, 0.1428312500, 0.0926562500];
+SER_MMSE_DOMIC_64_4=[0.2117062500, 0.1375937500, 0.0697531250 ,0.0310750000 ,0.0144656250 ,0.0057031250, 0.0025781250 ,0.0009750000 ];
+SER_MMSE_DOMIC_64_16=[0.7122281250, 0.6747156250, 0.6412843750, 0.6009187500, 0.5644312500, 0.5144531250 ,0.4591187500 ,...
+    0.3888562500, 0.3088750000];
+% fig1=figure;
+% Xsize=8.8;Ysize=11;
 % % Xleft=(8-Xsize)/2;Ytop=(12-Ysize)/2;
 % Xleft=0;
 % Ytop=0;
@@ -15,43 +54,117 @@ fig1=figure;
 % set(fig1,'position', [Xleft,Ytop,Xsize,Ysize]);
 % set(fig1,'PaperUnits','inches');
 % set(fig1,'PaperPosition',[Xleft,Ytop,Xsize,Ysize]);
-% set(gca,'fontsize',15);
+% set(gca,'fontsize',18);
 figure (1)
-fig1=semilogy([10:2:24],SER_MMSE_OSIC_32_4,'-*');xlabel('SNR(dB)'),ylabel('SER');
+semilogy([10:2:24]-3,SER_MMSE_OSIC_32_4,'-*', 'MarkerSize', 8);xlabel('SNR(dB)'),ylabel('SER');
 hold on;
-fig1=semilogy([10:2:28],SER_MMSE_OSIC_32_16,'-d','MarkerSize',15);xlabel('SNR(dB)'),ylabel('SER');
+semilogy([10:2:24]-3,SER_MMSE_OSIC_64_4,'-o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
 hold on;
-fig1=semilogy([10:2:24],SER_MMSE_OSIC_64_4,'-o','MarkerSize',15);xlabel('SNR(dB)'),ylabel('SER');
+semilogy([10:2:24]-3,SER_RSVD_OMIC_32_4,':*','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
 hold on;
-fig1=semilogy([10:2:24],SER_MMSE_OSIC_64_16,'->','MarkerSize',15);xlabel('SNR(dB)'),ylabel('SER');
+semilogy([10:2:20]-3,SER_RSVD_OMIC_64_4,':o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
 hold on;
+title('SER Versus SNR(dB) for 4QAM');
+hold on 
+legend('MMSE-OSIC-32','MMSE-OSIC-64','SVD-OMIC-32','SVD-OMIC-64');
+hold off
 
-fig1=semilogy([10:2:24],SER_RSVD_OMIC_32_4,':*','MarkerSize',15);xlabel('SNR(dB)'),ylabel('SER');
+
+figure (2)
+semilogy([10:2:28]-3,SER_MMSE_OSIC_32_16,'-d','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
 hold on;
-fig1=semilogy([10:2:28],SER_RSVD_OMIC_32_16,':d','MarkerSize',15);xlabel('SNR(dB)'),ylabel('SER');
+semilogy([10:2:34]-3,SER_MMSE_OSIC_64_16,'->','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
 hold on;
-fig1=semilogy([10:2:20],SER_RSVD_OMIC_64_4,':o','MarkerSize',15);xlabel('SNR(dB)'),ylabel('SER');
+semilogy([10:2:28]-3,SER_RSVD_OMIC_32_16,':d','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
 hold on;
-fig1=semilogy([10:2:24],SER_RSVD_OMIC_64_16,':>','MarkerSize',15);xlabel('SNR(dB)'),ylabel('SER');
+semilogy([10:2:34]-3,SER_RSVD_OMIC_64_16,':>','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
 hold on;
-% fig1=semilogy(0:2:18,sim8_16_0_18,'->','MarkerSize',15),xlabel('SNR per bit (dB)'),ylabel('BER');
-% fig1=semilogy(0:2:22,sim8_64_0_22,'-d','MarkerSize',15),xlabel('SNR per bit (dB)'),ylabel('BER');
+title('SER Versus SNR(dB) for 16QAM');
+hold on 
+legend('MMSE-OSIC-32','MMSE-OSIC-64','SVD-OMIC-32','SVD-OMIC-64');
+hold off
+
+figure (3)
+semilogy([10:2:30]-3,SER_MMSE_32_4,'-*', 'MarkerSize', 8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:30]-3,SER_MMSE_64_4,'-o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:28]-3,SER_RSVD_32_4,':*','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:26]-3,SER_RSVD_64_4,':o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+title('SER Versus SNR(dB) for 4QAM');
+hold on 
+legend('MMSE-32','MMSE-64','SVD-32','SVD-64');
+hold off
+
+figure (4)
+semilogy([10:2:30]-3,SER_MMSE_32_16,'-*', 'MarkerSize', 8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:30]-3,SER_MMSE_64_16,'-o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:28]-3,SER_RSVD_32_16,':*','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:28]-3,SER_RSVD_64_16,':o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+title('SER Versus SNR(dB) for 16QAM');
+hold on 
+legend('MMSE-32','MMSE-64','SVD-32','SVD-64');
+hold off
+
+figure (5)
+semilogy([10:2:28]-3,SER_MMSE_AOMIC_32_4,'-*', 'MarkerSize', 8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:28]-3,SER_MMSE_AOMIC_64_4,'-o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:28]-3,SER_MMSE_DOMIC_32_4,'--*', 'MarkerSize', 8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:24]-3,SER_MMSE_DOMIC_64_4,'--o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:24]-3,SER_RSVD_OMIC_32_4,':*','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:20]-3,SER_RSVD_OMIC_64_4,':o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+title('SER Versus SNR(dB) for 4QAM');
+hold on 
+legend('MMSE-AOMIC-32','MMSE-AOMIC-64','MMSE-DOMIC-32','MMSE-DOMIC-64','SVD-OMIC-32','SVD-OMIC-64');
+hold off
+
+figure (6)
+semilogy([10:2:32]-3,SER_MMSE_AOMIC_32_16,'-*', 'MarkerSize', 8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:28]-3,SER_MMSE_AOMIC_64_16,'-o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:30]-3,SER_MMSE_DOMIC_32_16,'--*', 'MarkerSize', 8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:26]-3,SER_MMSE_DOMIC_64_16,'--o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:28]-3,SER_RSVD_OMIC_32_16,':*','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+semilogy([10:2:34]-3,SER_RSVD_OMIC_64_16,':o','MarkerSize',8);xlabel('SNR(dB)'),ylabel('SER');
+hold on;
+title('SER Versus SNR(dB) for 16QAM');
+hold on 
+legend('MMSE-AOMIC-32','MMSE-AOMIC-64','MMSE-DOMIC-32','MMSE-DOMIC-64','SVD-OMIC-32','SVD-OMIC-64');
+hold off
+
+
+% fig1=semilogy(0:2:18,sim8_16_0_18,'->','MarkerSize',18),xlabel('SNR per bit (dB)'),ylabel('BER');
+% fig1=semilogy(0:2:22,sim8_64_0_22,'-d','MarkerSize',18),xlabel('SNR per bit (dB)'),ylabel('BER');
 % hold on;
-% fig1=semilogy(0:2:14,sim16_4_0_14,'-o','MarkerSize',15),xlabel('SNR per bit (dB)'),ylabel('BER');
+% fig1=semilogy(0:2:14,sim16_4_0_14,'-o','MarkerSize',18),xlabel('SNR per bit (dB)'),ylabel('BER');
 % hold on;
-% fig1=semilogy(0:2:18,sim16_16_0_18,'-s','MarkerSize',15),xlabel('SNR per bit (dB)'),ylabel('BER');
+% fig1=semilogy(0:2:18,sim16_16_0_18,'-s','MarkerSize',18),xlabel('SNR per bit (dB)'),ylabel('BER');
 % hold on;
-% fig1=semilogy(0:2:14,sim32_4_0_14,'-p','MarkerSize',15),xlabel('SNR per bit (dB)'),ylabel('BER');
+% fig1=semilogy(0:2:14,sim32_4_0_14,'-p','MarkerSize',18),xlabel('SNR per bit (dB)'),ylabel('BER');
 % hold on;
-% fig1=semilogy(6:2:22,sim_100_40_new,'-+','MarkerSize',15);xlabel('SNR (dB)'),ylabel('SER'),
+% fig1=semilogy(6:2:22,sim_100_40_new,'-+','MarkerSize',18);xlabel('SNR (dB)'),ylabel('SER'),
 % hold on;
-% fig1=semilogy(6:2:22,sim_100_100_new,'->','MarkerSize',15);xlabel('SNR (dB)'),ylabel('SER'),
+% fig1=semilogy(6:2:22,sim_100_100_new,'->','MarkerSize',18);xlabel('SNR (dB)'),ylabel('SER'),
 % hold on;
-% fig1=semilogy(6:2:24,sim_100_40_old,'-*','MarkerSize',15),xlabel('SNR (dB)'),ylabel('SER'),
+% fig1=semilogy(6:2:24,sim_100_40_old,'-*','MarkerSize',18),xlabel('SNR (dB)'),ylabel('SER'),
 % legend('8X8 4QAM','8X8 16QAM','8X8 64QAM','16X16 4QAM','16X16 16QAM','32X32 4QAM','48X48 4QAM');
-legend('MMSE-OSIC-32-4', 'MMSE-OSIC-32-16', 'MMSE-OSIC-64-4', 'MMSE-OSIC-64-16',...
-    'RSVD-OMIC-32-4', 'RSVD-OMIC-32-16', 'RSVD-OMIC-64-4', 'RSVD-OMIC-64-16');
-hold off;
+
 % print('-depsc','-tiff','-r300','BER_curves');
 % hold off;
 
